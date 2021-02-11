a08012cb80318a13ce6fd5b7eeedb2b44c3759a3
Safwat Imran <safwatimran@gmail.com>
2021-02-11 23:53:44 +0600

    'builtdatabase'

--- api.py
+++ api.py
@@ -1,10 +1,11 @@
-from flask import Flask 
+from flask import Flask, request, jsonify 
 from flask_sqlalchemy import SQLAlchemy
-
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config ['SECRET_KEY'] = 'secretkey'
-app.config ['SQLACLHEMY_DATABASE_URI']= 'sqlite:////C:\\Users\\User\\restful api\\todo.db'
+app.config['SECRET_KEY'] = 'secretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 
 db = SQLAlchemy(app)
 
@@ -20,6 +21,25 @@ class Todo(db.Model):
     text = db.Column(db.String(50))
     complete = db.Column(db.Boolean)
     user_id = db.Column(db.Integer)
+
+@app.route('/user', methods=['GET'])
+def get_all_users():
+    data = request.get_json()
+    
+    return ''
+
+@app.route('/user/<user_id>', methods=['POST'])
+def create_user():
+    return ''
+
+@app.route('/user/<user_id>', methods=['PUT'])
+def promote_user():
+    return ''
+
+@app.route('/user/<user_id>', methods=['DELETE'])
+def delete_user():
+    return ''
+
 
 if __name__ == '__main__':
     app.run(debug=True)
