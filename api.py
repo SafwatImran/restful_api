8635a5398579a559d3bf1b5a666288652e272986
Safwat Imran <safwatimran@gmail.com>
2021-02-12 01:33:56 +0600

    'added_login_route'

--- api.py
+++ api.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, jsonify 
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
+from functools import wraps
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
@@ -21,6 +24,8 @@ class Todo(db.Model):
     text = db.Column(db.String(50))
     complete = db.Column(db.Boolean)
     user_id = db.Column(db.Integer)
+
+
 
 @app.route('/user', methods=['GET'])
 def get_all_users():
@@ -77,6 +82,17 @@ def delete_user(public_id):
     db.session.commit()
     return jsonify({'message': 'User deleted'})
 
-
+@app.route('/login')
+def login ():
+    auth = request.authorization
+    if not auth or not auth.username or not auth.password:
+        return make_response("Could not verify1", 401, {'WWW-Authenticate': 'Basic realm = "Login required!"'})
+    user = User.query.filter_by(name = auth.username).first()
+    if not user: 
+        return make_response("Could not verify2", 401, {'WWW-Authenticate': 'Basic realm = "Login required!"'})
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'public_id': user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=5)}, app.config['SECRET_KEY'], algorithm='HS256')
+        return jsonify({'token': token.decode('UTF-8')})
+    return make_response("Could not verify3", 401, {'WWW-Authenticate': 'Basic realm = "Login required!"'})
 if __name__ == '__main__':
     app.run(debug=True)
